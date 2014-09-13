8996ffe8f421992e0ba4ecee880e5dd8c12eec1b
Wojciech Duda <wduda@wduda.com>
2014-09-13 18:18:15 +0200

    Fixed login form size by placing panel-login class to bootstrap panel div.

--- views/login.php
+++ views/login.php
@@ -1,7 +1,7 @@
 <?php defined('SYSPATH') or die('No direct script access.'); ?>
 <div class="row">
 	<div class="col-xs-offset-1 col-sm-offset-2 col-md-offset-4 col-xs-10 col-sm-8 col-md-4">
-	<div class="panel panel-default">
+	<div class="panel panel-default panel-login">
 		<div class="panel-heading">
 			<h3 class="panel-title">Logowanie</h3>
 		</div>
@@ -19,7 +19,7 @@
 			<?php echo Form::password('password', '', array('id' => 'password', 'class' => 'form-control input-lg')); ?>
 		</div>
 
-<?php echo Form::submit('sign_in', 'Zaloguj', array("class" => "btn btn-primary btn-lg center-block")); ?>
+<?php echo Form::submit('sign_in', 'Zaloguj', array("class" => "btn btn-primary btn-lg btn-block")); ?>
 <?php echo Form::close(); ?>
 		</div>
 	</div>
