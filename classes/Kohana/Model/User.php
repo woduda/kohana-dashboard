12fac19b999165a4fa66789322f09aab6625b4ec
Wojciech Duda <wduda@wduda.com>
2014-09-16 10:06:10 +0200

    Rewritten checking if user is active (less SQL queries by changing ORM flow to direct SQLs).

--- classes/Kohana/Model/User.php
+++ classes/Kohana/Model/User.php
@@ -4,27 +4,55 @@ class Kohana_Model_User extends Model_Auth_User {
 
 	const LOGIN_ROLE_ID = 1;
 
+	/**
+	 * Sets or gets if user is active.
+	 * Active means: "can login" = has login role
+	 * @param boolean $active
+	 * @throws Kohana_Exception
+	 * @return boolean|Kohana_Model_User
+	 */
 	public function active($active = NULL)
 	{
-		$login_role = ORM::factory('Role', array('name' => 'login'));
+		if ( ! $this->loaded())
+			throw new Kohana_Exception("Can't operatore on unloaded Model_User");
+
+		$has = $this->has_login_role();
 
 		// Getter:
 		if ($active === NULL)
-		{
-			return $this->has('roles', $login_role);
-		}
+			return $has;
 
 		// Setter:
-		if ($active AND ! $this->has('roles', $login_role))
+		if ($active AND ! $has)
 		{
-			$this->add('roles', $login_role);
+			// activate (add login role)
+			DB::insert("roles_users", array("user_id", "role_id"))
+				->values(array($this->id, Kohana_Model_User::LOGIN_ROLE_ID))
+				->execute($this->_db);
 		}
-		if ( ! $active AND $this->has('roles', $login_role))
+		if ( ! $active AND $has)
 		{
-			$this->remove('roles', $login_role);
+			// deactivate (remove login role)
+			DB::delete("roles_users")
+				->where("user_id", '=', $this->id)
+				->where("role_id", '=', Kohana_Model_User::LOGIN_ROLE_ID)
+				->execute($this->_db);
 		}
 
 		return $this;
+	}
+
+	public function has_login_role()
+	{
+		if ( ! $this->loaded())
+			throw new Kohana_Exception("Can't operatore on unloaded Model_User");
+
+		return (bool) DB::select(array(DB::expr('COUNT(*)'), 'total_count'))
+			->from("roles_users")
+			->where("user_id", '=', $this->id)
+			->where("role_id", '=', Kohana_Model_User::LOGIN_ROLE_ID)
+			->execute($this->_db)
+			->get('total_count');
 	}
 
 } // End Kohana_Model_User
